f520d570a4de5e3cfd620d0eed47e582f78b4188
CMullaney01 <conormullreid@live.com>
2024-02-09 01:42:03 +0000

    testfor email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,6 +19,10 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
+  console.log("ENV")
+  console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID)
+  console.log(import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID)
+  console.log(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY)
 
     emailjs
       .send(
